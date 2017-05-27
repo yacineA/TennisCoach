365588241573346f7b4d5b83209ef6ca0830c314
Konstantin <ak-iv@hotmail.com>
2017-05-27 17:17:22 +0200

    Test

---
+++ app/src/main/java/fr/utt/eg23/projet/tenniscoach/MenuPrincipal.java
@@ -0,0 +1,13 @@
+package fr.utt.eg23.projet.tenniscoach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
