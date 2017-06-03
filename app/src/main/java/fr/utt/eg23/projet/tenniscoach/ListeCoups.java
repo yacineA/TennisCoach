ec0b13fe48214db1ff8e760234b247d3b490b747
ANZAR Yassine <anzaryassine@gmail.com>
2017-06-03 23:45:10 +0200

    Initial commit

--- app/src/main/java/fr/utt/eg23/projet/tenniscoach/ListeCoups.java
+++ app/src/main/java/fr/utt/eg23/projet/tenniscoach/ListeCoups.java
@@ -4,8 +4,27 @@ package fr.utt.eg23.projet.tenniscoach;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AbsListView;
+        import android.widget.AdapterView;
         import android.widget.ArrayAdapter;
         import android.widget.ListView;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import java.util.ArrayList;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.AdapterView.OnItemClickListener;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import android.widget.ListView;
+        import android.view.View;
+
+
+        import java.util.ArrayList;
 
 public class ListeCoups extends AppCompatActivity {
 
@@ -15,16 +34,49 @@ public class ListeCoups extends AppCompatActivity {
             "Coup8", "Coup9", "Coup10", "Coup11", "Coup12", "Coup13",
             "Coup14", "Coup15"
     };
+    public ArrayList<String> selectedItems= new ArrayList<String>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_coups);
-        mListView = (ListView) findViewById(R.id.list);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(ListeCoups.this,
-                android.R.layout.simple_list_item_1, coups);
-        mListView.setAdapter(adapter);
-
     }
+
+
+    public void onStart(){
+        super.onStart();
+        //create an instance of ListView
+        ListView chl=(ListView) findViewById(R.id.checkable_list);
+        //set multiple selection mode
+        chl.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        //supply data itmes to ListView
+        ArrayAdapter<String> aa=new ArrayAdapter<String>(this,R.layout.rowlayout,R.id.txt_lan,coups);
+        chl.setAdapter(aa);
+        //set OnItemClickListener
+        chl.setOnItemClickListener(new OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // selected item
+                String selectedItem = ((TextView) view).getText().toString();
+                if(selectedItems.contains(selectedItem))
+                    selectedItems.remove(selectedItem); //remove deselected item from the list of selected items
+                else
+                    selectedItems.add(selectedItem); //add selected item to the list of selected items
+
+            }
+
+        });
+    }
+
+    public void showSelectedItems(View view){
+        String selItems="";
+        for(String item:selectedItems){
+            if(selItems=="")
+                selItems=item;
+            else
+                selItems+="/"+item;
+        }
+        Toast.makeText(this, selItems, Toast.LENGTH_LONG).show();
+    }
+
 }
--- app/src/main/java/fr/utt/eg23/projet/tenniscoach/ListeExercices.java
+++ app/src/main/java/fr/utt/eg23/projet/tenniscoach/ListeExercices.java
@@ -2,8 +2,14 @@ package fr.utt.eg23.projet.tenniscoach;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ListeExercices extends AppCompatActivity {
 
@@ -14,15 +20,49 @@ public class ListeExercices extends AppCompatActivity {
             "Exercice14", "Exercice15"
     };
 
+    public ArrayList<String> selectedItems= new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_exercices);
-        mListView = (ListView) findViewById(R.id.list);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(ListeExercices.this,
-                android.R.layout.simple_list_item_1, exos);
-        mListView.setAdapter(adapter);
-
     }
+
+
+    public void onStart(){
+        super.onStart();
+        //create an instance of ListView
+        ListView chl=(ListView) findViewById(R.id.checkable_list);
+        //set multiple selection mode
+        chl.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        //supply data itmes to ListView
+        ArrayAdapter<String> aa=new ArrayAdapter<String>(this,R.layout.rowlayout,R.id.txt_lan,exos);
+        chl.setAdapter(aa);
+        //set OnItemClickListener
+        chl.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // selected item
+                String selectedItem = ((TextView) view).getText().toString();
+                if(selectedItems.contains(selectedItem))
+                    selectedItems.remove(selectedItem); //remove deselected item from the list of selected items
+                else
+                    selectedItems.add(selectedItem); //add selected item to the list of selected items
+
+            }
+
+        });
+    }
+
+    public void showSelectedItems(View view){
+        String selItems="";
+        for(String item:selectedItems){
+            if(selItems=="")
+                selItems=item;
+            else
+                selItems+="/"+item;
+        }
+        Toast.makeText(this, selItems, Toast.LENGTH_LONG).show();
+    }
+
 }
